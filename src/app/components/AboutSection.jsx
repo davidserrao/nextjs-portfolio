e63753289294dfc53bd831aa7027dc7d2975e3bd
David Serrao <davidmserrao@gmail.com>
2023-09-19 11:36:49 -0400

    vercel optimizations 1

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -75,7 +75,7 @@ const AboutSection = () => {
   return (
     <section className='text-white' id="about">
         <div className="md:grid md:grid-cols-2 gap-8  py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-            <Image src="/images/about.jpeg" width={500} height={500} className="rounded hidden md:block lg:block "/>
+            <Image src="/images/about.jpeg" width={500} height={500} className="rounded hidden md:block lg:block" alt="mountain-pic"/>
             <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                 <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                 <p className='text-base lg:text-lg'>I’m currently a second year Computer Science student at Georgia Tech with experience in
